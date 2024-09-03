98b35c1f829219f5452bbd52020381da0950ad22
tuan vu <tuan.vu@tuni.fi>
2024-09-03 15:10:22 +0300

    Update read_data.py

--- src/filling_time_pred_src/Data/read_data.py
+++ src/filling_time_pred_src/Data/read_data.py
@@ -16,10 +16,10 @@ def read_data() -> pd.DataFrame:
         A Pandas DataFrame representing the content of the specified file.
     """
     print("update v1")
-    print("connecting to 194.157.214.66, 1028")
+    print("connecting to 192.168.100.201, 1028")
     try:
         # ping
-        r1 = requests.get(url="http://194.157.214.66:1028/")
+        r1 = requests.get(url="http://192.168.100.201:1028/")
         print(r1.status_code)
         print(r1.content)
     except Exception as err:
