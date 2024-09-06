f96bef2b1fb1452a4c7fdd415182e7dfff90e213
tuan vu <tuan.vu@tuni.fi>
2024-09-06 15:24:21 +0300

    Update read_data.py

--- src/filling_time_pred_src/Data/read_data.py
+++ src/filling_time_pred_src/Data/read_data.py
@@ -30,7 +30,7 @@ def read_data() -> pd.DataFrame:
         #if not using IDS, your own code
         # ADD YOUR OWN CODE
 
-
+        IP_addr = "130.230.140.135"
     
         #if using IDS 
         #Uncomment this block and set the parameters
@@ -38,7 +38,8 @@ def read_data() -> pd.DataFrame:
         #<<Dataset Provider Port>>: None if any forwarding of the default port 8086 in the dataset provider connector has been done when deploying otherwise forwarding port
         ids_agent_client = IDSAgentClient()
         # #Start transfer dataset
-        resp= ids_agent_client.get_asset_from_ids("CLARUS_HK_UC1_SC1_filling_time_v2","130.230.140.135","3040")
+        print("connecting to",IP_addr)
+        resp= ids_agent_client.get_asset_from_ids("CLARUS_HK_UC1_SC1_filling_time_v2",IP_addr,"3040")
         if resp == False:
             print("unable to connect")
             return None
