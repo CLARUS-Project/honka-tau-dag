3471495b1d8d599fb86360eb05b4d8f328090ecb
tuan vu <tuan.vu@tuni.fi>
2024-09-25 15:34:18 +0300

    Update read_data.py

--- src/filling_time_pred_src/Data/read_data.py
+++ src/filling_time_pred_src/Data/read_data.py
@@ -19,7 +19,7 @@ def read_data() -> pd.DataFrame:
     #print("connecting to 194.157.214.66, 1028")
 
         # ping
-    r1 = requests.get(url="http://194.157.214.66:1028/")
+    r1 = requests.get(url="http://194.157.214.74:1028/")
     print(r1.status_code)
     print(r1.content)
     #except Exception as err:
