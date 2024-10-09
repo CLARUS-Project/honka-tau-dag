39036faf2ce38d24507ae8a140318a3f730c9899
tuan vu <tuan.vu@tuni.fi>
2024-10-09 14:59:14 +0300

    Update read_data.py

--- src/filling_time_pred_src/Data/read_data.py
+++ src/filling_time_pred_src/Data/read_data.py
@@ -33,8 +33,8 @@ def read_data() -> pd.DataFrame:
         #if not using IDS, your own code
         # ADD YOUR OWN CODE
 
-        IP_addr = "130.230.140.135"
-        #IP_addr = "194.157.214.74"
+        #IP_addr = "130.230.140.135"
+        IP_addr = "194.157.214.74"
         print("connecting to ", IP_addr)
     
         #if using IDS 
@@ -44,7 +44,7 @@ def read_data() -> pd.DataFrame:
         ids_agent_client = IDSAgentClient()
         # #Start transfer dataset
         #print("connecting to",IP_addr)
-        resp= ids_agent_client.get_asset_from_ids(config.MLFLOW_EXPERIMENT,connectorIP=IP_addr, connectorPort="3040")
+        resp= ids_agent_client.get_asset_from_ids(config.MLFLOW_EXPERIMENT,connectorIP=IP_addr, connectorPort="8086")
         if resp == False:
             print("unable to connect")
             return None
