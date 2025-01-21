383d6ec8e5cab286ac0eb088f27ae6a8ad15910f
tuan vu <tuan.vu@tuni.fi>
2025-01-21 15:51:08 +0200

    Update read_data.py

--- src/filling_time_pred_src/Data/read_data.py
+++ src/filling_time_pred_src/Data/read_data.py
@@ -44,18 +44,25 @@ def read_data() -> pd.DataFrame:
         ids_agent_client = IDSAgentClient()
         # #Start transfer dataset
         #print("connecting to",IP_addr)
-        resp= ids_agent_client.get_asset_from_ids(config.MLFLOW_EXPERIMENT,connectorIP=IP_addr, connectorPort="8086")
-        if resp == False:
-            print("unable to connect")
-            return None
-
-        else:
-            #Get dataset from agent volume
-            print("connected, get data")
-            response=ids_agent_client.get_dataset(config.MLFLOW_EXPERIMENT)
+        try:
+            print("get existing data")
+            response = ids_agent_client.get_dataset(config.MLFLOW_EXPERIMENT)
             data = StringIO(response)
             df = pd.read_csv(data, delimiter=';', quotechar='"')
             return df
+        except:
+            resp= ids_agent_client.get_asset_from_ids(config.MLFLOW_EXPERIMENT,connectorIP=IP_addr, connectorPort="8086")
+            if resp == False:
+                print("unable to connect")
+                return None
+
+            else:
+                #Get dataset from agent volume
+                print("connected, get data")
+                response=ids_agent_client.get_dataset(config.MLFLOW_EXPERIMENT)
+                data = StringIO(response)
+                df = pd.read_csv(data, delimiter=';', quotechar='"')
+                return df
 
        
     
