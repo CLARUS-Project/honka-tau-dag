1a346a7c722f0aaae24d0afc264aa583a2d92c47
Tuan Vu (TAU) <tuan.vu@tuni.fi>
2025-02-19 13:17:03 +0200

    multiple_dag

---
+++ src/filling_time_pred_v2_src/Data/read_data.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from io import StringIO
+from ids_read.ids_agent_client  import IDSAgentClient
+import config
+import requests
+def read_data() -> pd.DataFrame:
+
+    """
+    The function implements the logic to ingest the data and transform it into a pandas format.
+
+    In this code example, a csv file is retrieved from a datasource.
+    If not using IDS add your own code to read the datasource
+    If using IDS uncomment the example code and replace <<Dataset Provider IP>> with the IP of the dataset provider partipant
+
+    Return:
+        A Pandas DataFrame representing the content of the specified file.
+    """
+    #print("update v1")
+    #print("connecting to 194.157.214.66, 1028")
+
+        # ping
+
+    #print("CONNECTION TEST")
+    #try:
+    #    r1 = requests.get(url="http://194.157.214.74:1028")
+    #    print(r1.content)
+    #except:
+    #    print("cannot connect")
+    #print("CONNECTION TEST END")
+
+    try:
+
+        #if not using IDS, your own code
+        # ADD YOUR OWN CODE
+
+        #IP_addr = "130.230.140.135"
+        IP_addr = "194.157.214.74"
+        print("connecting to ", IP_addr)
+    
+        #if using IDS 
+        #Uncomment this block and set the parameters
+        #<<Dataset Provider IP>>: IP of the host where the dataset provider connector is deployed
+        #<<Dataset Provider Port>>: None if any forwarding of the default port 8086 in the dataset provider connector has been done when deploying otherwise forwarding port
+        ids_agent_client = IDSAgentClient()
+        # #Start transfer dataset
+        #print("connecting to",IP_addr)
+        try:
+            print("get existing data")
+            response = ids_agent_client.get_dataset(config.MLFLOW_EXPERIMENT)
+            data = StringIO(response)
+            df = pd.read_csv(data, delimiter=';', quotechar='"')
+            return df
+        except:
+            resp= ids_agent_client.get_asset_from_ids(config.MLFLOW_EXPERIMENT,connectorIP=IP_addr, connectorPort="8086")
+            if resp == False:
+                print("unable to connect")
+                return None
+
+            else:
+                #Get dataset from agent volume
+                print("connected, get data")
+                response=ids_agent_client.get_dataset(config.MLFLOW_EXPERIMENT)
+                data = StringIO(response)
+                df = pd.read_csv(data, delimiter=';', quotechar='"')
+                return df
+
+       
+    
+    except Exception as exc:
+        print(f'error:  { str(exc)}') 
+        return None
+       
---
+++ src/filling_time_pred_v2_src/ids_read/ids_agent_client.py
@@ -0,0 +1,127 @@
+import requests
+from typing import Optional
+
+
+
+
+from typing import Any, Optional
+from io import StringIO
+
+
+class IDSAgentClient:
+    def __init__(self):
+        pass
+        
+    def get(self, url: str,timeout:int) -> Optional[requests.Response]:
+        """_summary_
+
+        Args:
+            url (str): _description_
+            timeout (int): _description_
+
+        Returns:
+            Optional[requests.Response]: _description_
+        """
+        try:
+          
+            ret = requests.get(url, verify= False, timeout=timeout)
+            """  logger.debug("GET returned {}".format(ret)) """
+            return ret
+        except requests.RequestException as e:
+            print(
+                "Request error {} on GET REQUEST to {}".format(type(e).__name__, url)
+            )
+            return None
+        
+    def get_asset_from_ids_using_minio(self, expId:str, connectorIP:str,minioEndpoint:str,minioUser:str,minioPass:str) -> bool:
+        """_summary_
+
+        Args:
+            expId (str): _description_
+            connectorIP (str): _description_
+            minioEndpoint (str): _description_
+            minioUser (str): _description_
+            minioPass (str): _description_
+
+        Returns:
+            bool: _description_
+        """
+        try:
+            # #Query connector consumer to get dataset artifact from connector provider
+            # url = "http://34.250.205.215:8082/api/v2/consumer/asset?exp_id="+expId+"&asset_type=dataset&provider_ip="+connectorIP
+            # response = self.get(url,120)
+            # #Check operation result
+            # if response is None or response != 200:
+            #     return False
+            # else:
+            #     #Get csv from minio server
+            #     # Connect to minio
+            #     client = Minio(minioEndpoint, access_key= minioUser, secret_key=minioPass,secure=False)
+            #     # Download asset to shared folder (connector datalake)
+            #     minio_path =expId + "/dataset.csv"
+            #     dest_path = "dataset.csv"
+            #     client.fget_object("dataset", minio_path, dest_path) 
+                return True
+            
+        except Exception as e:
+           
+            return False
+        
+    def get_asset_from_ids(self, expId:str, connectorIP:str, connectorPort: Optional[str] =None) -> bool:
+        """_summary_
+
+        Args:
+            expId (str): _description_
+            connectorIP (str): _description_
+
+        Returns:
+            bool: _description_
+        """
+        try:
+            #Query connector consumer to get dataset artifact from connector provider
+            if connectorPort is None:            
+                url = "http://34.250.205.215:8082/api/v2/consumer/asset?exp_id="+expId+"&asset_type=dataset&provider_ip="+connectorIP
+            else:
+                url = "http://34.250.205.215:8082/api/v2/consumer/asset?exp_id="+expId+"&asset_type=dataset&provider_ip="+connectorIP+"&provider_port="+connectorPort
+            response = self.get(url,120)
+            print("get_asset_from_ids resp ", response)
+            #Check operation result
+            if response is None or response.status_code != 200:
+                return False
+            else:
+                return True
+            
+        except Exception as e:
+           
+            return False        
+        
+    def get_dataset(self, expId:str):
+
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        try:
+            #Query agent to get dataset saved in volume 
+            url = "http://34.250.205.215:8082/api/v2/dataset?exp_id="+expId
+            #url = "http://localhost:8082/api/v2/dataset?exp_id="+expId
+            #url = "http://34.250.205.215:8082/api/v2/consumer/asset?exp_id=" + expId + "&asset_type=dataset&provider_ip=" + connectorIP + "&provider_port=" + connectorPort
+            #url = "http://34.250.205.215:8082/api/v2/dataset?exp_id=" + expId + "&asset_type=dataset&provider_ip=" + connectorIP + "&provider_port=" + connectorPort
+            response = self.get(url,120)
+
+            #Check operation result
+            if response is None or response.status_code != 200:
+                print("data retrieve failed")
+                return ""
+            else:
+
+                resp = response.json()
+                print("retrieved data",resp)
+                print("retrieved data end")
+                data = resp["message"]
+                return data
+            
+        except Exception as e:
+            print("error", e)
+            return False
